2bc8c490061c81cd5a44484fe350e3671c7f4c34
hydropyrum <375114+hydropyrum@users.noreply.github.com>
2025-09-10 22:31:29 -0400

    integers mod m

--- ts/ring.test.ts
+++ ts/ring.test.ts
@@ -1,4 +1,17 @@
-import { RingElement, Ring, Integer, ZZ } from './ring';
+import { RingElement, Ring, Integer, ZZ, IntegersMod} from './ring';
 
 test('TestInteger', () => {
+});
+
+let ZZmod10 = new IntegersMod(10n);
+function mod10(n) { return ZZmod10.fromInt(n); }
+
+test('TestIntegerMod', () => {
+    expect(mod10(5).add(mod10(7))).toStrictEqual(mod10(2));
+    expect(mod10(5).sub(mod10(7))).toStrictEqual(mod10(8));
+    expect(mod10(5).mul(mod10(7))).toStrictEqual(mod10(5));
+    expect(mod10(1).div(mod10(3))).toStrictEqual(mod10(7));
+    expect(() => mod10(1).div(mod10(2))).toThrow("Division by zero");
+    expect(mod10(3).inv()).toStrictEqual(mod10(7));
+    expect(() => mod10(2).inv()).toThrow("Division by zero");
 });
--- ts/ring.ts
+++ ts/ring.ts
@@ -78,3 +78,91 @@ export class Integers implements Ring<Integer> {
 }
 
 export const ZZ = new Integers();
+
+export class IntegerMod implements RingElement<IntegerMod> {
+    m: bigint;
+    n: bigint;
+    constructor(n: bigint, m: bigint) {
+        this.n = n;
+        this.m = m;
+        this.normalize();
+    }
+    clone(): IntegerMod { return new IntegerMod(this.n, this.m); }
+    private check(y: IntegerMod) {
+        if (this.m != y.m) throw RangeError("numbers have different moduli");
+    }
+    private normalize() { this.n = ((this.n % this.m) + this.m) % this.m; }
+    
+    equals(y: IntegerMod): boolean { this.check(y); return this.n == y.n; }
+    toString(): string { return String(this.n); }
+    toNumber(): number { return Number(this.n); }
+    
+    iadd(y: IntegerMod): IntegerMod {
+        this.check(y);
+        this.n += y.n;
+        this.normalize();
+        return this;
+    }
+    add(y: IntegerMod): IntegerMod { return this.clone().iadd(y); }
+    isub(y: IntegerMod): IntegerMod {
+        this.check(y);
+        this.n -= y.n;
+        this.normalize();
+        return this;
+    }
+    sub(y: IntegerMod): IntegerMod { return this.clone().isub(y); }
+    neg(): IntegerMod { return new IntegerMod(-this.n, this.m); }
+    imul(y: IntegerMod): IntegerMod {
+        this.check(y);
+        this.n *= y.n;
+        this.normalize();
+        return this;
+    }
+    mul(y: IntegerMod): IntegerMod { return this.clone().imul(y); }
+
+    idiv(y: IntegerMod): IntegerMod {
+        this.check(y);
+        let [r, s, t] = extended_gcd(y.n, this.m);
+        if (this.n % r == 0n) {
+            this.n = s * (this.n / r);
+            this.normalize();
+            return this;
+        } else
+            throw new RangeError("Division by zero");
+    }
+    div(y: IntegerMod): IntegerMod { return this.clone().idiv(y); }
+    inv(): IntegerMod {
+        let [r, s, t] = extended_gcd(this.n, this.m);
+        if (r == 1n) {
+            return new IntegerMod(s, this.m)
+        } else
+            throw new RangeError("Division by zero");
+    }
+
+    // These don't make sense mod m
+    sign(): number { throw TypeError('unsupported operation') }
+    abs(): IntegerMod { throw TypeError('unsupported operation') }
+    compare(y: IntegerMod): number { throw TypeError('unsupported operation') }
+}
+
+export class IntegersMod implements Ring<IntegerMod> {
+    m: bigint;
+    constructor(m: bigint) { this.m = m; }
+    zero(): IntegerMod { return new IntegerMod(0n, this.m); }
+    one(): IntegerMod { return new IntegerMod(0n, this.m); }
+    fromInt(n: number): IntegerMod { return new IntegerMod(BigInt(n), this.m); }
+}
+
+export function extended_gcd(a: bigint, b: bigint) {
+    /* Extended Euclid: Find r, s, t such that r = as + bt */
+    let [r0, r1] = [a, b];
+    let [s0, s1] = [1n, 0n];
+    let [t0, t1] = [0n, 1n];
+    while (r1 != 0n) {
+        let [q, r2] = [r0/r1, r0%r1];
+        [r0, r1] = [r1, r2];
+        [s0, s1] = [s1, s0 - q*s1];
+        [t0, t1] = [t1, t0 - q*t1];
+    }
+    return [r0, s0, t0];
+}
